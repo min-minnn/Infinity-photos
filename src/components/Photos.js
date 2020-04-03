d05d11bb17c23495dbf781c93ccf40754345efd0
min-minnn <alswlk0306@gmail.com>
2020-04-03 13:06:57 +0900

    Infinite scroll photo album

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import styled from 'styled-components';
 
 const ImgBlock = styled.div`
+  height: 100%;
+  overflow: auto;
   img{
     &:hover {
       filter: brightness(50%);
@@ -15,36 +17,38 @@ const Img = styled.img`
   margin-bottom: 1rem;
 `;
 
-function Photos(deps =[]) {
-  const [photos, setPhotos] = useState("");
-  const [pageNumber, setPageNumber] = useState(1);
+function Photos() {
+  const [photos, setPhotos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchPhotos = async() => {
-    try{
-      setPhotos(null);
-      setError(null);
+  const fetchMorePhotos = () => {
+    setTimeout(() => {
       setLoading(true);
-
-      const response = await axios.get(
-        "https://api.unsplash.com/photos?page="+pageNumber+"&client_id=YUWTnOVlEviY4Y8uDNYkcDpbivf760DA3CmTjOZKXzw"
-      );
-      setPhotos(response.data);
-    } catch (e) {
-      setError(e);
-    }
+      try {
+        axios.get('https://api.unsplash.com/photos/random', {
+          params: {
+            client_id: 'XjG_79dJXuU7Zx5TRHZBBmSTuqTdcAs_LtyesusCJNU',
+            count: 30
+          }
+        })
+        .then((response) => {
+          setPhotos(photos.concat(response.data));
+        }); 
+      } catch (e) {
+        setError(e);
+      }
     setLoading(false);
-}
+    }, 2000)
+  };
 
   useEffect(() => {
-    fetchPhotos();
+    fetchMorePhotos();
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps);
+  }, [photos])
 
-  if(loading) return <div>loading</div>
-  if(error) return <div>error</div>
-  if(!photos) return null;
+  if(loading) return <h4>loading...</h4>;
+  if(error) return <h4>error</h4>
 
   return(
     <ImgBlock>
